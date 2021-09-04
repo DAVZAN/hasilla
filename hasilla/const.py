1b0dbd58613a28cf4b8aa675d86b4062d41a305d
Davide Zanatta <zanatta.davide703@gmail.com>
2021-09-04 10:59:33 +0200

    Created MQTT Client class

--- hasilla/const.py
+++ hasilla/const.py
@@ -1,6 +1,9 @@
 """Silla constants."""
 from typing import Final
 
+
+COMMAND_BACKLOG: Final = "Backlog"
+
 CONF_MANUFACTURER: Final = "manufacturer"
 CONF_NAME: Final = "name"
 CONF_MODEL: Final = "md"
